7abc911f9ca73a02fa947897fa5d57a5bc105c89
AndrewSwinney <andrewswinney14@gmail.com>
2023-11-17 10:13:30 +0000

    Initial commit

---
+++ src/main/java/com/barclays/model/Book.java
@@ -0,0 +1,34 @@
+package com.barclays.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Book {
+
+    @Id
+    @GeneratedValue
+
+    private int id;
+
+    private int isbn;
+
+    private String title;
+
+    private String author;
+
+    private int yearPublished;
+
+
+}
