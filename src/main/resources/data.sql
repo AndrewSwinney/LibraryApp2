3038138e7ef0ac093b2d5a76c43b8d151cb23c86
AndrewSwinney <andrewswinney14@gmail.com>
2023-11-17 15:51:06 +0000

    Added a new data entry for Movies.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,6 @@ insert into Book (id, isbn, title, genre, author, year_published) values (1, 123
 
 insert into Movie (id, title, lead_actor, director, screen_writer, release_year, genre, rating) values
 (1, 'Goodfellas', 'Ray Liotta', 'Martin Scorsese', 'Martin Scorsese & Nicholas Pileggi', 1990, 'Crime', 8.7);
+
+insert into Movie (id, title, lead_actor, director, screen_writer, release_year, genre, rating) values
+(2, 'Titanic', 'Leonardo DiCaprio & Kate Winslet', 'James Cameron', 'James Cameron', 1997, 'Romance', 7.9);
--- src/test/java/com/barclays/controller/BookControllerIntegrationTests.java
+++ src/test/java/com/barclays/controller/BookControllerIntegrationTests.java
@@ -1,7 +1,6 @@
 package com.barclays.controller;
 
 import com.barclays.model.Book;
-import com.barclays.model.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
